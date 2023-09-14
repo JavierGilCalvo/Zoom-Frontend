b10b86556d3f531bfc84b3cb4037175c9d5b4853
Javier Gil Calvo <jgil@isdefe.es>
2023-09-14 11:45:11 +0200

    Change Endpoint

--- src/services/isBrowserZoom.js
+++ src/services/isBrowserZoom.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
+const ENDPOINT_ZOOM_BACKEND = 'https://wijjw4sa31.execute-api.us-east-1.amazonaws.com/dev/'
 export const getTextToShow = async () => {
     try {
-        const textToShow = await axios.get('/')
-        return textToShow
+        const response = await axios.get(ENDPOINT_ZOOM_BACKEND)
+        return response.message
     } catch (error) {
         console.error(error)
     }
