6ac2060dc17c0fcba6a18a1a8facb604b25aeae2
Javier Gil Calvo <jgil@isdefe.es>
2023-09-15 15:00:47 +0200

    Prueba Ditto

--- src/services/isBrowserZoom.js
+++ src/services/isBrowserZoom.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
 
 const ENDPOINT_ZOOM_BACKEND = 'https://wijjw4sa31.execute-api.us-east-1.amazonaws.com/dev/'
+const ENDPOINT_PRUEBA_DITTO = 'https://pokeapi.co/api/v2/pokemon/ditto'
 export const getTextToShow = async () => {
   try {
     const response = await axios.get(ENDPOINT_ZOOM_BACKEND)
     return response.data.message
   } catch (error) {
     console.error(error)
+    const response = await axios.get(ENDPOINT_PRUEBA_DITTO)
+    return response.data.name
   }
 }
