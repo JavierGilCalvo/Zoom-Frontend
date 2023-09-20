b9a182be5078133bd0a3e17e49e7cd4cbc7dc04e
13JaviG <13javiergc@gmail.com>
2023-09-20 20:43:47 +0200

    Request undefined

--- src/services/isBrowserZoom.js
+++ src/services/isBrowserZoom.js
@@ -4,10 +4,10 @@ const ENDPOINT_ZOOM_BACKEND = 'https://wijjw4sa31.execute-api.us-east-1.amazonaw
 // const ENDPOINT_PRUEBA_DITTO = 'https://pokeapi.co/api/v2/pokemon/ditto'
 export const getTextToShow = async () => {
   try {
-        const response = await fetch(ENDPOINT_ZOOM_BACKEND, { mode: 'cors' }) //axios.get(ENDPOINT_ZOOM_BACKEND);
-        console.log(response)
-        return response.data.message
-    } catch (error) {
-      return `Error Request ${JSON.stringify(error.request)}`
-    }
+    const response = await fetch(ENDPOINT_ZOOM_BACKEND, { mode: 'cors' }) // axios.get(ENDPOINT_ZOOM_BACKEND);
+    console.log(response)
+    return response.data.message
+  } catch (error) {
+    return `Error Request ${JSON.stringify(error)}`
+  }
 }
