e7476fe50fa85fe3299704546a5534effcea3f5a
Javier Gil Calvo <jgil@isdefe.es>
2023-09-20 14:13:44 +0200

    Error track Headers

--- src/services/isBrowserZoom.js
+++ src/services/isBrowserZoom.js
@@ -8,6 +8,12 @@ export const getTextToShow = async () => {
           method: 'get',
           url: ENDPOINT_ZOOM_BACKEND,
           withCredentials: false,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'origin':'x-requested-with',
+            'Access-Control-Allow-Headers': 'POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin',
+            'Content-Type': 'application/json'
+        }
         })
         console.log(response)
         return response.data.message
