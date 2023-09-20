3302d209826509ea4dcc0f58e40bee9f4dcdc12e
Javier Gil Calvo <jgil@isdefe.es>
2023-09-20 13:33:18 +0200

    Error track

--- src/services/isBrowserZoom.js
+++ src/services/isBrowserZoom.js
@@ -2,17 +2,23 @@ import axios from 'axios'
 
 const ENDPOINT_ZOOM_BACKEND = 'https://wijjw4sa31.execute-api.us-east-1.amazonaws.com/dev/'
 const ENDPOINT_PRUEBA_DITTO = 'https://pokeapi.co/api/v2/pokemon/ditto'
-export const getTextToShow = async () => {
-  try {
-    const response = await axios.get(ENDPOINT_ZOOM_BACKEND)
-    return response.data.message
-  } catch (error) {
-    console.error(error)
-    try {
-      const response = await axios.get(ENDPOINT_PRUEBA_DITTO)
-      return response.data.name
-    } catch (err) {
-      return JSON.stringify(error, Object.getOwnPropertyNames(err))
+export const getTextToShow = () => {
+  let respuesta = {}
+  axios.get(ENDPOINT_ZOOM_BACKEND).then(response => {
+    console.log(response);
+    respuesta = respuesta.data.message
+  })
+  .catch(error => {
+    if (error.response) {
+        console.log('Data', error.response.data);
+        console.log('Status', error.response.status);
+        console.log('Headers', error.response.headers);
+        respuesta = `Data: ${error.response.data} - Status: ${error.response.status} - Headers: ${error.response.headers}`
+    } else if (error.request) {
+        console.log('Error Request', error.request);
+    } else {
+        console.log('Error', error.message);
     }
-  }
+  })
+  return respuesta
 }
